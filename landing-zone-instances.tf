44557de1fcc762b9a6bd554b6400aea7af5660b9
Michael Griehm <griehmmj@gmail.com>
2022-03-07 16:51:46 -0500

    Fix github repo name.

--- landing-zone-instances.tf
+++ landing-zone-instances.tf
@@ -20,7 +20,7 @@ module "data_model_demo_landing_zone" {
   source                    = "./modules/landing-zone"
   app_name                  = "data-model"
   github_organization_name  = "michael-griehm"
-  github_repo_name          = "azure-data-engineer-DP203"
+  github_repo_name          = "azure-data-engineering-DP203"
   admin_user_principal_name = "mikeg@ish-star.com"
   env                       = "demo"
   github_bind_object        = "ref:refs/heads/main"
