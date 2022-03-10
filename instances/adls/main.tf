894a6577119167b3169a50fa150a544693c1ae26
Michael Griehm <griehmmj@gmail.com>
2022-03-10 13:58:47 -0500

    Added Azure Data Lake Storage Gen 2 Demo Landing Zone.

---
+++ instances/adls/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azuread = "~>2.16.0"
+  }
+
+  backend "azurerm" {
+  }
+}
+
+module "data_brick_demo_landing_zone" {
+  source                    = "../../modules/landing-zone"
+  app_name                  = "adls"
+  github_organization_name  = "michael-griehm"
+  github_repo_name          = "azure-data-lake"
+  admin_user_principal_name = "mikeg@ish-star.com"
+  env                       = "demo"
+  github_bind_object        = "ref:refs/heads/main"
+
+  tags = {
+    environment = "demo"
+    workload    = "crypto-analytics"
+  }
+}
